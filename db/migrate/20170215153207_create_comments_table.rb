3ea7a06bdd94671626f63c9f347e0c8d1ff9856d
Jordforb@gmail.com <Jordanforbes1@Jordans-MacBook-Pro-2.local>
2017-02-15 10:59:56 -0500

    text files

--- db/migrate/20170215153207_create_comments_table.rb
+++ db/migrate/20170215153207_create_comments_table.rb
@@ -3,7 +3,7 @@ class CreateCommentsTable < ActiveRecord::Migration[5.0]
   	create_table :comments do |t|
   		t.text :body
   		t.integer :user_id
-
+  		t.integer :post_id
   		t.timestamps
   	end
   end
